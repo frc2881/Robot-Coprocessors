7fc726980458c80c07abdf4c5b15b4cb95c2c805
Robert Tuttle <frc2881@gmail.com>
2024-03-22 20:33:11 -0500

    Update main.py

--- Robot-Lights/main.py
+++ Robot-Lights/main.py
@@ -16,14 +16,13 @@ nt.setServer("10.28.81.2", ntcore.NetworkTableInstance.kDefaultPort4)
 lightsModeTopic = nt.getStringTopic("/SmartDashboard/Robot/Lights/Mode").subscribe("Default")
 
 colorHotPink = color.calculate_intensity((150, 0, 15), 1)
-colorOrange = color.calculate_intensity((255, 40, 0), 1)
-colorGreen = color.calculate_intensity((0, 255, 0), 1)
 
 pixels = neopixel.NeoPixel(BOARD_PIN, LIGHTS_COUNT, brightness=1, auto_write=False, pixel_order=neopixel.GRB)
 
 default = comet.Comet(pixels, speed=0.015, color=colorHotPink, tail_length=24)
-intakeReady = chase.Chase(pixels, speed=0.02, color=colorOrange, size=6, spacing=6)
-launchReady = pulse.Pulse(pixels, speed=0.1, color=colorGreen, period=0.2)
+intakeNotReady = chase.Chase(pixels, speed=0.02, color=color.BLUE, size=6, spacing=6)
+intakeReady = chase.Chase(pixels, speed=0.02, color=color.ORANGE, size=6, spacing=6, reverse=True)
+launchReady = pulse.Pulse(pixels, speed=0.1, color=color.GREEN, period=0.2)
 
 def onExit():
   pixels.fill(color.BLACK)
